ff9f07ac4a7136cd15c87425af166c77268eff0d
DESKTOP-7VC0Q42\os <kanokporn.tien@bumail.net>
2021-01-25 18:27:39 +0700

    Send Assignment Week1

---
+++ GI455_Project/Assets/Scripts/GetInput.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GetInput : MonoBehaviour
+{
+    public InputField CheckWords;
+    string words;
+    public Text WordText;
+
+    void Update()
+    {
+        words = CheckWords.text;
+    }
+
+    public void GetInputOnClick()
+    {
+        if (words == "Seasons")
+        {
+            WordText.text = $"<Color=green>{words}</color>" + " is found.";
+            Debug.Log(CheckWords.text + " is found.");
+        }
+        else if (words == "Autumn")
+        {
+            WordText.text = $"<Color=green>{words}</color>" + " is found.";
+            Debug.Log(CheckWords.text + " is found.");
+        }
+        else if (words == "Spring")
+        {
+            WordText.text = $"<Color=green>{words}</color>" + " is found.";
+            Debug.Log(CheckWords.text + " is found.");
+        }
+        else if (words == "Winter")
+        {
+            WordText.text = $"<Color=green>{words}</color>" + " is found.";
+            Debug.Log(CheckWords.text + " is found.");
+        }
+        else if (words == "Summer")
+        {
+            WordText.text = $"<Color=green>{words}</color>" + " is found.";
+            Debug.Log(CheckWords.text + " is found.");
+        }
+        else 
+        {
+            WordText.text = $"<Color=red>{words}</color>" + " is not found.";
+            Debug.Log(CheckWords.text + " is not found.");
+        }
+    }
+}
