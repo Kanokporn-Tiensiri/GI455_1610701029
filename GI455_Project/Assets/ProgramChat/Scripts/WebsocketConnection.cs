c2ebf937b8c89a2cc99dd79908acea9f3a043f69
DESKTOP-7VC0Q42\os <kanokporn.tien@bumail.net>
2021-02-04 00:20:22 +0700

    Send Assignment Week2

--- GI455_Project/Assets/ProgramChat/Scripts/WebsocketConnection.cs
+++ GI455_Project/Assets/ProgramChat/Scripts/WebsocketConnection.cs
@@ -35,30 +35,14 @@ namespace ProgramChat
 
         void Update()
         {
-            /*if (Input.GetKeyDown(KeyCode.Return)) //Press Enter to send
-            {
-                //websocket.Send("Rendom number : " + Random.Range(0, 999999));
-                if (websocket.ReadyState == WebSocketState.Open)
-                {
-                    //newMSG = InputMes.text; //Show text output
-                    websocket.Send(InputUser.text + " : " + InputMes.text);
-                    var newMSG = Instantiate(ChatBox, BGBox);
-                    newMSG.GetComponent<Text>().text = InputUser.text + ":" + InputMes.text + "\n"; //Chat box move up
-                    newMSG.transform.SetSiblingIndex(0);
-                    newMSG.GetComponent<Text>().alignment = TextAnchor.LowerRight;
-                }
-            }*/
-
-            //MessageEventArgs messageEventArgs;
-            //var newMSG = Instantiate(ChatBox, BGBox);
-            //newMSG.GetComponent<Text>().text = messageEventArgs.Data + "\n"; //Chat box move up
-            //string[] msg = messageEventArgs.Data.Split(':');
-            //newMSG.transform.SetSiblingIndex(0);
-
-            //if (InputUser.text + " " == msg[0])
+            //if (Input.GetKeyDown(KeyCode.Return)) //Press Enter to send
             //{
-            //    //User.text = InputUser.text;
-            //    newMSG.GetComponent<Text>().alignment = TextAnchor.LowerRight;
+            //        websocket.Send("Rendom number : " + Random.Range(0, 999999));
+            //    if (websocket.ReadyState == WebSocketState.Open)
+            //    {
+            //        //newMSG = InputMes.text; //Show text output
+            //        websocket.Send(InputUser.text + " : " + InputMes.text);
+            //    }
             //}
         }
 
@@ -68,11 +52,6 @@ namespace ProgramChat
             {
                 //Mes.text = InputMes.text;
                 websocket.Send(InputUser.text + " : " + InputMes.text);
-                /*var newMSG = Instantiate(ChatBox, BGBox);
-                newMSG.GetComponent<Text>().text = InputUser.text + ":" + InputMes.text + "\n"; //Chat box move up
-                newMSG.transform.SetSiblingIndex(0);
-                newMSG.GetComponent<Text>().alignment = TextAnchor.LowerRight;
-                print(InputUser.text + " : " + InputMes.text);*/
             }
         }
 
